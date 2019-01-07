29cbc3ab13ab6f6e5ba5311c12bce50195c9494d
Dominique RIGHETTO <dominique.righetto@gmail.com>
2019-01-07 10:17:09 +0100

    Fix an error in the verification script

--- scripts/Apply_Linter_Check.sh
+++ scripts/Apply_Linter_Check.sh
@@ -6,7 +6,7 @@ markdownlint -o linter-result.out cheatsheets
 errors=`wc -m linter-result.out | cut -d' ' -f1`
 if [[ $errors != "0" ]]
 then
-    echo "[!] $errors error(s) found by the Linter:"
+    echo "[!] Error(s) found by the Linter:"
     cat linter-result.out
     exit $errors
 else
