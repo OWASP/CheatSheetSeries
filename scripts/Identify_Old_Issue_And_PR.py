09ac894a9b597162e096f9223f3b4fa19cf942e7
Dominique RIGHETTO <dominique.righetto@gmail.com>
2019-08-25 18:35:49 +0200

    Enhance the rendering of the issues/pr detected.

--- scripts/Identify_Old_Issue_And_PR.py
+++ scripts/Identify_Old_Issue_And_PR.py
@@ -12,7 +12,7 @@ Python3 script to identify any Issue or PR meeting the following criterias:
 
 Do not require to have a local copy of the GitHub repository.
 
-Dependencies: pip install requests
+Dependencies: pip install requests 
 """
 import sys
 import requests
@@ -21,8 +21,9 @@ from datetime import datetime
 
 # Define constants
 ## API to retrieve the list of Issues/PR (GitHub REST API v3 considers every pull request an issue, but not every issue is a pull request)
+## Ask to the API to sort the list by the updated date in order to have the oldest on the top on the list
 ## See https://developer.github.com/v3/issues/#list-issues-for-a-repository for explanation
-ISSUE_API = "https://api.github.com/repos/OWASP/CheatSheetSeries/issues?page=1&per_page=1000"
+ISSUE_API = "https://api.github.com/repos/OWASP/CheatSheetSeries/issues?page=1&per_page=1000&sort=updated&direction=asc"
 ## Expiration delay
 MAX_MONTHS_ALLOWED = 1
 
@@ -48,8 +49,8 @@ def is_old_pull_request(issue):
             break
     return has_waiting_for_update_label
 
-# Grab the list of Issues/PR
-print("[+] Grab the list of Issues/PR via the GitHub API...")
+# Grab the list of open Issues/PR
+print("[+] Grab the list of open Issues/PR via the GitHub API...")
 response = requests.get(ISSUE_API)
 if response.status_code != 200:
     print("Cannot load the list of Issues/PR content: HTTP %s received!" % response.status_code)
@@ -65,19 +66,20 @@ for issue in issues:
     last_update = datetime.strptime(issue["updated_at"], "%Y-%m-%dT%H:%M:%SZ")
     diff_in_months = round(abs((datetime.today() - last_update).days / 30))
     if diff_in_months > MAX_MONTHS_ALLOWED:
-        id = issue["number"]
+        id = str(issue["number"])
         if "pull_request" in issue and is_old_pull_request(issue):
             old_issues["PR"].append(id)
         elif is_old_issue(issue):
             old_issues["ISSUE"].append(id)
 
 # Render the result
-print("[+] State:")
 return_code = len(old_issues["PR"]) + len(old_issues["ISSUE"])
-if len(old_issues["PR"]) > 0:
-    print("Old pull request identified: %s" % old_issues["PR"])
-if len(old_issues["ISSUE"]) > 0:
-    print("Old issue identified: %s" % old_issues["ISSUE"])
-if return_code == 0:
-    print("Nothing identified.")
+if return_code != 0:
+    print("[+] State:")
+    if len(old_issues["PR"]) > 0:
+        print("Old pull request identified (%s items): %s" % (len(old_issues["PR"]), " / ".join(old_issues["PR"])))
+    if len(old_issues["ISSUE"]) > 0:
+        print("Old issue identified (%s items): %s" % (len(old_issues["ISSUE"]), " / ".join(old_issues["ISSUE"])))
+else:
+    print("[+] State: Nothing identified.")
 sys.exit(return_code)
