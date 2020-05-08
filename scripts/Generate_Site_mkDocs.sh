a70ed9df852121d3b2cb0e25d7694fd3749f2759
Nicolas Dujardin <nicolas.dujardin@gmail.com>
2020-05-08 13:32:39 +0200

    fixes

--- scripts/Generate_Site_mkDocs.sh
+++ scripts/Generate_Site_mkDocs.sh
@@ -6,78 +6,73 @@
 
 GENERATED_SITE=site
 WORK=../generated
+date=$(date '+%Y-%m-%d')
+
 echo "Generate a offline portable website with all the cheat sheets..."
 
-echo "Step 1/7: Init work folder."
+echo "Step 1/6: Init work folder."
 rm -rf $WORK 1>/dev/null 2>&1
 mkdir $WORK
 mkdir $WORK/cheatsheets
 
-echo "Step 2/7: Generate the summary markdown page "
+echo "Step 2/6: Generate the summary markdown page "
 python Update_CheatSheets_Index.py
-python Generate_CheatSheets_TOC.py
 python Generate_RSS_Feed.py
 
-echo "Step 3/7: Create the expected MkDocs folder structure."
+echo "Step 3/6: Create the expected MkDocs folder structure."
 
 cp ../mkdocs.yml $WORK/.
-cp ../Preface.md $WORK/cheatsheets/.
-mv TOC.md $WORK/cheatsheets/.
+cp ../Preface.md $WORK/cheatsheets/index.md
 mv News.xml $WORK/cheatsheets/.
 cp -r ../cheatsheets $WORK/cheatsheets/cheatsheets
 cp -r ../assets $WORK/cheatsheets/assets
-cp ../Index.md $WORK/cheatsheets/cheatsheets/Index.md
-cp ../IndexASVS.md $WORK/cheatsheets/cheatsheets/IndexASVS.md
-cp ../IndexProactiveControls.md $WORK/cheatsheets/cheatsheets/IndexProactiveControls.md
-
+cp ../Index.md $WORK/cheatsheets/glossary.md
+cp ../IndexASVS.md $WORK/cheatsheets/IndexASVS.md
+cp ../IndexProactiveControls.md $WORK/cheatsheets/IndexProactiveControls.md
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
         # Mac OSX
-    sed -i '' 's/assets\//..\/assets\//g' $WORK/cheatsheets/cheatsheets/Index.md
-    sed -i '' 's/assets\//..\/assets\//g' $WORK/cheatsheets/cheatsheets/IndexASVS.md
-    sed -i '' 's/assets\//..\/assets\//g' $WORK/cheatsheets/cheatsheets/IndexProactiveControls.md
-    sed -i '' 's/cheatsheets\///g' $WORK/cheatsheets/cheatsheets/Index.md
-    sed -i '' 's/cheatsheets\///g' $WORK/cheatsheets/cheatsheets/IndexASVS.md
-    sed -i '' 's/cheatsheets\///g' $WORK/cheatsheets/cheatsheets/IndexProactiveControls.md
+    sed -i '' 's/Index.md/glossary.md/g' $WORK/cheatsheets/glossary.md
+    sed -i '' "1i\\
+        Title: Index Alphabetical\\
+        Date: $date\\
+        " $WORK/cheatsheets/glossary.md
+    sed -i '' "1i\\
+        Title: Index ASVS\\
+        Date: $date\\
+        " $WORK/cheatsheets/IndexASVS.md
+    sed -i '' "1i\\
+        Title: Index Proactive Controls\\
+        Date: $date\\
+        " $WORK/cheatsheets/IndexProactiveControls.md
+
 else
-    sed -i 's/assets\//..\/assets\//g' $WORK/cheatsheets/cheatsheets/Index.md
-    sed -i 's/assets\//..\/assets\//g' $WORK/cheatsheets/cheatsheets/IndexASVS.md
-    sed -i 's/assets\//..\/assets\//g' $WORK/cheatsheets/cheatsheets/IndexProactiveControls.md
-    sed -i 's/cheatsheets\///g' $WORK/cheatsheets/cheatsheets/Index.md
-    sed -i 's/cheatsheets\///g' $WORK/cheatsheets/cheatsheets/IndexASVS.md
-    sed -i 's/cheatsheets\///g' $WORK/cheatsheets/cheatsheets/IndexProactiveControls.md
+    sed -i 's/Index.md/glossary.md/g' $WORK/cheatsheets/glossary.md
+    sed -i "1iTitle: Index Alphabetical\nDate: $date\n" $WORK/cheatsheets/glossary.md
+    sed -i "1iTitle: Index ASVS\nDate: $date\n" $WORK/cheatsheets/IndexASVS.md
+    sed -i "1iTitle: Index Proactive Controls\nDate: $date\n" $WORK/cheatsheets/IndexProactiveControls.md
 fi
 
 
-echo "Step 4/7: Inserting markdown metadata."
+echo "Step 4/6: Inserting markdown metadata."
 for fullfile in $WORK/cheatsheets/cheatsheets/*.md
 do
+    filename=$(basename -- "$fullfile")
+    filename="${filename%.*}"
+
+    echo "Processing file: $fullfile"
     if [[ "$OSTYPE" == "darwin"* ]]; then
         # Mac OSX
-        filename=$(basename -- "$fullfile")
-        filename="${filename%.*}"
-        date=$(date '+%Y-%m-%d')
-        echo "Processing file: $fullfile"
         sed -i '' "1i\\
             Title: ${filename//[_]/ }\\
             Date: $date\\
             " $fullfile
     else
-        filename=$(basename -- "$fullfile")
-        filename="${filename%.*}"
-        date=$(date '+%Y-%m-%d')
-        echo "Processing file: $fullfile"
         sed -i "1iTitle: ${filename//[_]/ }\nDate: $date\n" $fullfile
     fi
 done
 
-echo "Step 5/7: FavIcon"
-
-mkdir $WORK/cheatsheets/img
-cp ../assets/WebSite_Favicon.png $WORK/cheatsheets/img/apple-touch-icon-precomposed-152.png
-cp ../assets/WebSite_Favicon.ico $WORK/cheatsheets/img/favicon.ico
-
-echo "Step 6/7: Generate the site."
+echo "Step 5/6: Generate the site."
 
 cd $WORK
 python -m mkdocs build
@@ -88,7 +83,7 @@ then
     exit 1
 fi
 
-echo "Step 7/7 Cleanup."
+echo "Step 6/6 Cleanup."
 rm -rf cheatsheets
 rm mkdocs.yml
 
