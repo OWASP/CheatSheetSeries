d4dab1a8525833db9d094e2ecaa0c218f76faec5
Uncle Joe <1244005+sydseter@users.noreply.github.com>
2026-02-27 22:18:45 +0100

    Update Generate_Site.sh

--- scripts/Generate_Site.sh
+++ scripts/Generate_Site.sh
@@ -25,6 +25,7 @@ cp -r ../cheatsheets $WORK/cheatsheets/cheatsheets
 cp -r ../assets $WORK/cheatsheets/assets
 cp ../Index.md $WORK/cheatsheets/cheatsheets/Index.md
 cp ../IndexASVS.md $WORK/cheatsheets/cheatsheets/IndexASVS.md
+cp ../IndexASVS4.md $WORK/cheatsheets/cheatsheets/IndexASVS4.md
 cp ../IndexMASVS.md $WORK/cheatsheets/cheatsheets/IndexMASVS.md
 cp ../IndexProactiveControls.md $WORK/cheatsheets/cheatsheets/IndexProactiveControls.md
 cp ../IndexTopTen.md $WORK/cheatsheets/cheatsheets/IndexTopTen.md
@@ -34,6 +35,7 @@ sed -i 's/assets\//..\/assets\//g' $WORK/cheatsheets/cheatsheets/IndexMASVS.md
 sed -i 's/assets\//..\/assets\//g' $WORK/cheatsheets/cheatsheets/IndexTopTen.md
 sed -i 's/cheatsheets\///g' $WORK/cheatsheets/cheatsheets/Index.md
 sed -i 's/cheatsheets\///g' $WORK/cheatsheets/cheatsheets/IndexASVS.md
+sed -i 's/cheatsheets\///g' $WORK/cheatsheets/cheatsheets/IndexASVS4.md
 sed -i 's/cheatsheets\///g' $WORK/cheatsheets/cheatsheets/IndexMASVS.md
 sed -i 's/cheatsheets\///g' $WORK/cheatsheets/cheatsheets/IndexProactiveControls.md
 sed -i 's/cheatsheets\///g' $WORK/cheatsheets/cheatsheets/IndexTopTen.md
